ff8b989b87f5608714593f7ddb97b9b4671d7104
Bobbyann <158587071+Bobbyann@users.noreply.github.com>
2024-03-08 14:48:57 +0000

    updated script file

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ document.getElementById('funButton').addEventListener('click', function() {
     const funText = document.getElementById('funText');
     funText.innerHTML = '🎉 Congratulations on taking your first step into web development with CS 140! 🎉';
 });
+
+console.log("hello world");
